853962427df7e56dad3ba50c6ad62e41cc4010ff
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-10-10 20:46:04 +0300

    Changes labels and announcements text into English

--- src/announcement-data.js
+++ src/announcement-data.js
@@ -1,44 +1,44 @@
  export const announcementsData = [
     {
       id: 1,
-      title: 'Новый год 2021',
-      description: 'В течение новогодних и рождественских праздников в городе планируют провести ряд тематических спектаклей, концертов и вертепов. Все эти события будут происходить с соблюдением карантинных норм. Львов делает главный акцент в праздновании на традиционных мероприятиях, таких как открытие шопки или Дидуха.',
+      title: 'New Year 2021',
+      description: 'During the New Year and Christmas holidays, the city is planning to host a number of thematic performances, concerts and nativity scenes. All these events will take place in compliance with quarantine standards. Lviv focuses on traditional celebrations such as the opening of a shop or Didukh.',
       data: '30.11.2020',
       coverImage: 'https://24tv.ua/resources/photos/news/768x432_DIR/202012/1492698.jpg?v=1608649327000&q=50'},
     {
       id: 2,
-      title: 'Новый год 2022',
-      description: 'Открытие рождественской шопки перед входом в Ратушу состоится 24 декабря в 18:00. В мероприятии примет участие группа KYRBASY. В свою очередь, установление Дидуха традиционно состоится 6 января в 12:00 на площади Яворского с участием хора "Гомон".В Шевченковском гаю проведут действо "Рождество в Гаю".',
+      title: 'New Year 2022',
+      description: 'The opening of the Christmas shop in front of the entrance to the Town Hall will take place on December 24 at 18:00. The KYRBASY group will take part in the event. In turn, the installation of Didukh will traditionally take place on January 6 at 12:00 on Yavorsky Square with the participation of the "Gomon" choir.',
       data: '01.05.2021',
       coverImage: 'https://hotel-edem.lviv.ua/wp-content/uploads/2018/09/golovna.jpg'},
     {
       id: 3,
-      title: 'Концерт Pianoboy',
-      description: 'На осінньому концерті Дмитро Шуров і його гурт виконають всі головні хіти наживо. Композиції, що вже полюбилися нам, отримають несподіване оркестрове аранжування. Як це буде? Дізнаємося осінню. Не пропустіть особливе шоу, в якому сплетуться поп, рок, симфонічна музика і ультрасучасна електроніка. Приєднуйтесь до яскравої і дружної pianofamily.',
+      title: 'Pianoboy сoncert',
+      description: "At the autumn concert, Dmitri Shurov and his band will perform all major hits live. Songs that become hits and fallen in heart of thousands will reveal in an unexpected arrangement. Don’t miss special show, which combines pop, rock, symphonic music and ultra-modern electronics. Join the bright and friendly piano family",
       data: '10.02.2021',
       coverImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ6ioRhIgMUfZHUF0UKe_ft3rKxPYpVyIJ2w&usqp=CAU'},
     {
       id: 4,
-      title: 'Концерт Олега Винника',
-      description: 'Цієї осені Артист №1 в Україні Олег Винник дасть серію концертів з авторською програмою «Безумна Любов». На вас чекають легендарні хіти, які співає вся країна, потужна енергетика і душевна атмосфера.',
+      title: 'Concert of Oleg Vinnik',
+      description: "This fall, Artist №1 in Ukraine Oleh Vynnyk will give a series of concerts with the author's program 'Mad Love'. Legendary hits are sung by the whole country, powerful energy and emotional atmosphere.",
       data: '24.04.2021',
       coverImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfV5CEmMgOcPlTXKFnmf4kUdP8BURFa0V6Gw&usqp=CAU'},
     {
       id: 5,
-      title: 'Концерт гурту TVORCHI',
-      description: '1 жовтня запрошуємо на унікальний концерт популярного дуету TVORCHI із шоу-програмою ROAD!Центральний меседж нового альбому TVORCHI – важливо насолоджуватися життям щомиті, а не рахувати кроки від однієї мети до іншої.',
+      title: 'TVORCHI concert',
+      description: 'On October 1, we invite you to a unique concert of the popular duo TVORCHI with the show ROAD! The central message of the new TVORCHI album is important to enjoy life every moment, not to count steps from one goal to another.',
       data: '29.06.2021',
       coverImage: 'https://yt3.ggpht.com/C5pPYcMFg371E8SKbBoTzDdZwQVwLuXrwY9EEBQiiL6pqF9S4RgbqzUvIwjiT9GAedTESS-e=s900-c-k-c0x00ffffff-no-rj'},
     {
       id: 6,
-      title: 'Виставка «Суспільство з кіноапаратом»',
-      description: 'Виставка причинить дверцята приватних відеоколекцій львівян та мешканців інших міст. ',
+      title: 'Exhibition "Society with a Film Camera"',
+      description: 'The exhibition will open the doors of private video collections of Lviv residents and residents of other cities.',
       data: '06.07.2021',
       coverImage: 'https://i.ytimg.com/vi/9H-Jn-23Ph0/maxresdefault.jpg'},
     {
       id: 7,
-      title: 'Год Тельца',
-      description: 'Виставка причинить дверцята приватних відеоколекцій львівян та мешканців інших міст. ',
+      title: 'Year of Taurus',
+      description: 'This year you should be more careful with investments and family relationships. We also advise you to try yourself in art.',
       data: '06.07.2021',
       coverImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXp7583P9DR1cJiRVbWfgty-72D_w51IB_xA&usqp=CAU'}
   ]
